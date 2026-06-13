6df4ddbae42c865b53d93a2a6c1cb8ff89173276
Virencq <23637556+Virencq@users.noreply.github.com>
2026-06-13 19:46:37 +0530

    add coinswitch_client/__init__.py

---
+++ coinswitch_client/__init__.py
@@ -0,0 +1,13 @@
+from coinswitch_client.APIClient import CoinSwitchClient
+
+
+def v2_instant(api_key="", secret_key="", ip="1.1.1.1"):
+    return CoinSwitchClient.v2_instant(api_key=api_key, secret_key=secret_key, ip=ip)
+
+
+def v2_fixed(api_key="", secret_key="", ip="1.1.1.1"):
+    return CoinSwitchClient.v2_fixed(api_key=api_key, secret_key=secret_key, ip=ip)
+
+
+def v1(api_key="", secret_key="", ip="1.1.1.1"):
+    return CoinSwitchClient.v1(api_key=api_key, secret_key=secret_key, ip=ip)
